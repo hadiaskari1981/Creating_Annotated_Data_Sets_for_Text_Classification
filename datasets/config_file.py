ec97909cb58e152f513c16debbe0a58cc2d489b5
Hadi Askari <hadi.askari1981@gmail.com>
2019-05-20 23:39:55 +0200

    Delete config_file.py

--- datasets/config_file.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DOWNLOAD_ROOT = "https://raw.githubusercontent.com/hadiaskari1981/sentiment_analysis/master/"
-DATASETS_PATH = os.path.join("datasets")
-DOWNLOAD_URL = DOWNLOAD_ROOT + "datasets/datasets.tar.gz"
-TWEETS_PATH = os.path.join("datasets", "tweets")
-TRAINING_DATA_URL = "http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip"
